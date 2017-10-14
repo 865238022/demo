a6e5d1431115d3f1cb883243a2fad6f07670e1a3
陈顺 <13786285072@163.com>
2017-10-14 12:22:44 +0800

    Create Test.java

---
+++ IdeaProjects/demo/src/main/java/top/huzhurong/scheduled/Test.java
@@ -0,0 +1,19 @@
+package top.huzhurong.scheduled;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by 竹 on 2017/10/14.
+ */
+@Component
+public class Test implements IMyTestService {
+    @Scheduled(cron="0/10 * *  * * ? ")   //每5秒执行一次
+    @Override
+    public void myTest(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(getClass().getName() +"进入测试" +simpleDateFormat.format(new Date()));
+    }
+}
